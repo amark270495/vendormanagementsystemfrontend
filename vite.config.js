f2fbf5f9e9527876d1fe0f9056e9238ae1d2fcc5
dashboardtaproot <121403593+amark270495@users.noreply.github.com>
2025-08-02 11:19:24 +0530

    New Updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this line
-  base: '/vendormanagementsystemfrontend/', 
+  // The 'base' property should be removed for Azure deployment
   plugins: [react()],
 })
