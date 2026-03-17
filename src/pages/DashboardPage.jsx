9053d92dc11e0cfbbbd2c074dd27d8e3af46150e
amark270495 <ashokamarendra05@gmail.com>
2026-03-17 21:04:37 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -570,14 +570,17 @@ const DashboardPage = () => {
                         className="w-full pl-10 pr-4 py-2 bg-white border border-slate-300 rounded-xl text-sm focus:ring-4 focus:ring-indigo-500/10 focus:border-indigo-500 transition-all shadow-sm placeholder:text-slate-400 font-medium"
                     />
                 </div>
+                <div className="relative w-full md:w-96">
+                    <p>Status</p>
+                </div>
                 <select 
                     value={statusFilter} 
                     onChange={(e) => setStatusFilter(e.target.value)} 
                     className="w-full md:w-auto bg-white border border-slate-300 rounded-xl px-4 py-2 text-sm font-semibold text-slate-700 focus:ring-4 focus:ring-indigo-500/10 focus:border-indigo-500 shadow-sm transition-all"
                 >
                     <option value="">All Statuses</option>
-                    <option value="Open">Status: Open</option>
-                    <option value="Closed">Status: Closed</option>
+                    <option value="Open">Open</option>
+                    <option value="Closed">Closed</option>
                 </select>
             </div>
 
